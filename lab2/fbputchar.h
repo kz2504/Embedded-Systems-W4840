b13f3614e8b019f130088f4193de3e48a3804a52
Kuan Zhang <kz2504@columbia.edu>
2026-03-05 15:01:58 -0500

    test

--- lab2/fbputchar.h
+++ lab2/fbputchar.h
@@ -10,5 +10,6 @@
 extern int fbopen(void);
 extern void fbputchar(char, int, int);
 extern void fbputs(const char *, int, int);
+extern void draw_underline_cursor_locked(cursor_row, cursor_col);
 
 #endif
