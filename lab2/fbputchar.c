793df8f1674ded13572418d7f775a6a4a1ccde7a
Kuan Zhang <kz2504@columbia.edu>
2026-03-05 14:59:21 -0500

    bugfix

--- lab2/fbputchar.c
+++ lab2/fbputchar.c
@@ -112,6 +112,28 @@ void fbputs(const char *s, int row, int col)
   while ((c = *s++) != 0) fbputchar(c, row, col++);
 }
 
+static void draw_underline_cursor_locked(int row, int col)
+{
+  int bpp = BITS_PER_PIXEL / 8;          // should be 4
+  int cell_w = FONT_WIDTH * 2;
+  int cell_h = FONT_HEIGHT * 2;
+
+  // bottom scanline inside the cell (last row of pixels)
+  int y = (row * cell_h + fb_vinfo.yoffset) + (cell_h - 2); // -2 makes it 2px thick
+  unsigned char *p = framebuffer
+    + y * fb_finfo.line_length
+    + (col * cell_w + fb_vinfo.xoffset) * bpp;
+
+  // draw a 2-pixel-thick underline
+  for (int dy = 0; dy < 2; dy++) {
+    unsigned char *q = p + dy * fb_finfo.line_length;
+    for (int x = 0; x < cell_w; x++) {
+      q[0] = 255; q[1] = 255; q[2] = 255; q[3] = 0; // white
+      q += 4;
+    }
+  }
+}
+
 /* 8 X 16 console font from /lib/kbd/consolefonts/lat0-16.psfu.gz
 
 od --address-radix=n --width=16 -v -t x1 -j 4 -N 2048 lat0-16.psfu
